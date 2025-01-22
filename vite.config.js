9075d909c93db25f2da86b55bb990bfd591213c5
kwiaciarniamilosc <kwiaciarniamilosc@gmail.com>
2025-01-22 22:19:09 +0000

    latestone F

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Ensures relative paths for assets
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  }
-})
+});
