515c6e2dbefc490c29d8088482c2ebcc3fd9a8f4
kwiaciarniamilosc <kwiaciarniamilosc@gmail.com>
2025-01-22 21:51:35 +0000

    feedback page react

---
+++ feedback-app/src/App.jsx
@@ -0,0 +1,11 @@
+import FeedbackForm from './FeedbackForm'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8 px-4">
+      <FeedbackForm />
+    </div>
+  )
+}
+
+export default App
